75d177c13a4ba646fcebc64955153a36bee977b9
Varsine <56913551+Varsine@users.noreply.github.com>
2025-10-20 20:55:41 +0400

    Add comment about className naming conventions
    Added a comment regarding className naming conventions.

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -6,6 +6,7 @@ const MainComponent = () => {
   const products = ["Milk", "Bread", "Apple"];
 
   return (
+    // className-ը այսիսի անուն չպետք է ունենա
     <div className="p-4 border rounded-2xl shadow-md">
       <h2>Main Component (Parent)</h2>
       <ChildComponent user={user} products={products} />
