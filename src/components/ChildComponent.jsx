a941eb4fb494d43a402313abd3150c4620e659ce
Varsine <56913551+Varsine@users.noreply.github.com>
2025-10-20 20:53:43 +0400

    Add comments for styling and key usage in ChildComponent
    Added comments to clarify style guidelines and key usage.

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const ChildComponent = ({ user, products }) => {
   return (
+
+    // ինլայն սթայլներ չենք գրում, ստեղծիր առանձին սթայլ ֆայլեր
     <div style={{ marginTop: "10px" }}>
       <h3>Child Component (Props Received)</h3>
       <p> User: {user.name}, Age: {user.age}</p>
       <p> Products:</p>
       <ul>
+
+        //ինդեքսը չենք տալիս որպես key
         {products.map((p, i) => (
           <li key={i}>{p}</li>
         ))}
